f075a1b972a993a584e61df6a04c30ac7caca00e
Tao Bian <biantao0519@hotmail.com>
2022-12-31 22:23:32 +1100

    create Die.js and add style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react' 
+import Die from './Die'
 
 export default function App() {
     return (
         <main>
             <div className="container">
-                <div className="game"></div>
+                <div className="game">
+                    <Die />
+                </div>
             </div>
         </main>
     )
---
+++ src/Die.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Die() {
+    return (
+        <>
+            <div className='die'>1</div>
+            <div className='die'>1</div>
+            <div className='die'>1</div>
+            <div className='die'>1</div>
+            <div className='die'>1</div>
+            <div className='die'>1</div>
+            <div className='die'>1</div>
+            <div className='die'>1</div>
+            <div className='die'>1</div>
+            <div className='die'>1</div>
+        </>
+    )
+}
