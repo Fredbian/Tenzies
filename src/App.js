4164c791c5c7166ae6ab4557ac65999dab11ace4
Tao Bian <biantao0519@hotmail.com>
2023-01-01 08:37:32 +1100

    creat random function and render nums

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react' 
+import React, { useState } from 'react'
 import Die from './Die'
 
 export default function App() {
+
+    const [dice, setDice] = useState(allNewDice())
+
+    // create a random num function to return an array that hold 10 nums between 1-6
+    function allNewDice() {
+        const array = []
+        for (let i = 0; i < 10; i++) {
+            array.push(Math.ceil(Math.random() * 6))
+        }
+        return array
+    }
+
+    const die = dice.map((item, index) => {
+        return (
+            <Die key={index} value={item} />
+        )
+    })
+
+
     return (
         <main>
             <div className="container">
                 <div className="game">
-                    <Die value="1"/>
-                    <Die value="9"/>
-                    <Die value="6"/>
-                    <Die value="1"/>
-                    <Die value="2"/>
-                    <Die value="3"/>
-                    <Die value="4"/>
-                    <Die value="6"/>
-                    <Die value="4"/>
-                    <Die value="4"/>
+                    {die}
                 </div>
             </div>
         </main>
